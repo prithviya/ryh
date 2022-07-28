721083947a9202088f11fc401ab8642bafe3e95e
prithviya <94467336+prithviya@users.noreply.github.com>
2022-07-28 16:32:19 +0530

    7 commit

--- src/component/Client/Client.js
+++ src/component/Client/Client.js
@@ -8,33 +8,33 @@ function Client() {
             <h1 className='text-center'> 
                 Our Happy Clients
             </h1>
-            <div className='row pt-5 text-center'>
-                <div className='col-md-4 col-6 cust-card-border'>
+            <div className='row text-center'>
+                <div className='col-md-2 col-6 cust-card-border'>
                     <div className='custom-img'>
                         <img src={ Images.ANGLOFONE } style={{ width:'100%', height:'100%' }}/>
                     </div>                    
                 </div>
-                <div className='col-md-4 col-6 cust-card-border'>
+                <div className='col-md-2 col-6 cust-card-border'>
                     <div className='custom-img'>
                         <img src={ Images.AIC } style={{ width:'100%', height:'100%' }}/>
                     </div>                    
                 </div>
-                <div className='col-md-4 col-6 cust-card-border'>
+                <div className='col-md-2 col-6 cust-card-border'>
                     <div className='custom-img'>
                         <img src={ Images.FLUID } style={{ width:'100%', height:'100%' }}/>
                     </div>                    
                 </div>
-                <div className='col-md-4 col-6 cust-card-border'>
+                <div className='col-md-2 col-6 cust-card-border'>
                     <div className='custom-img'>
                         <img src={ Images.SHELTERS } style={{ width:'100%', height:'100%' }}/>
                     </div>                    
                 </div>                
-                <div className='col-md-4 col-6 cust-card-border'>
+                <div className='col-md-2 col-6 cust-card-border'>
                     <div className='custom-img'>
                         <img src={ Images.SR} style={{ width:'100%', height:'100%' }}/>
                     </div>                    
                 </div>
-                <div className='col-md-4 col-6 cust-card-border'>
+                <div className='col-md-2 col-6 cust-card-border'>
                     <div className='custom-img'>
                         <img src={ Images.RGI } style={{ width:'100%', height:'100%' }}/>
                     </div>                    
