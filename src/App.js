45f2a47021577b43dc01d7a9a313a582708aae47
prithviya <94467336+prithviya@users.noreply.github.com>
2022-11-03 11:45:13 +0530

    CSS CHANGES

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner from './component/Banner/Banner';
 import About from './component/About/About';
 import Contact from './component/Contact/contact';
-import Service from './component/Service/Service';
+// import Service from './component/Service/Service';
 import Arrive from './component/Arrive/Arrive';
 import Work from './component/Work/work';
 import Ongoing from './component/Ongoing/Ongoing';
@@ -16,9 +14,10 @@ import Client from './component/Client/Client';
 import Header from './component/Header/Header';
 import Team from './component/Team/Team';
 
-function App() {
+function App() 
+{
   return (
-    <div>
+    <div>  
       <div id="home">
         <Header/>
       </div>
@@ -55,7 +54,8 @@ function App() {
       <div id="">
         <Footer/>
       </div> 
-          {/* <Service/> */}      
+          {/* <Service/> */}
+             
     </div>
   );
 }
